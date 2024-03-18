0c2cfbb8e7d9e8eb4b9f31f80bb3914e0ca2793e
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-18 10:48:44 +0100

    feat: green testTake

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -1,10 +1,9 @@
 package u03
 
-import u02.AnonymousFunctions.l
 import u03.Optionals.Optional
 
 object Sequences: // Essentially, generic linkedlists
-  
+
   enum Sequence[E]:
     case Cons(head: E, tail: Sequence[E])
     case Nil()
@@ -13,29 +12,34 @@ object Sequences: // Essentially, generic linkedlists
 
     def sum(l: Sequence[Int]): Int = l match
       case Cons(h, t) => h + sum(t)
-      case _          => 0
+      case _ => 0
 
     def map[A, B](l: Sequence[A])(mapper: A => B): Sequence[B] = l match
       case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil()      => Nil()
+      case Nil() => Nil()
 
     def filter[A](l1: Sequence[A])(pred: A => Boolean): Sequence[A] = l1 match
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t)            => filter(t)(pred)
-      case Nil()                 => Nil()
+      case Cons(_, t) => filter(t)(pred)
+      case Nil() => Nil()
 
     // Lab 03
+    def take[A](l: Sequence[A])(n: Int): Sequence[A] =
+      l match
+        case Nil() => Nil()
+        case _ if n == 0 => Nil()
+        case Cons(h, t) => Cons(h, take(t)(n - 1))
+
     def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
 
-    def take[A](l: Sequence[A])(n: Int): Sequence[A] = ???
-    
     def concat[A](l1: Sequence[A], l2: Sequence[A]): Sequence[A] = ???
+
     def flatMap[A, B](l: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
 
     def min(l: Sequence[Int]): Optional[Int] = ???
-    
+
 @main def trySequences =
-  import Sequences.* 
+  import Sequences.*
   val l = Sequence.Cons(10, Sequence.Cons(20, Sequence.Cons(30, Sequence.Nil())))
   println(Sequence.sum(l)) // 30
 
