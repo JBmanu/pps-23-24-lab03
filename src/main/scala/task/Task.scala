72695063e93746272ab393e3ee0467a4c424506b
Manuel Buizo <65312924+JBmanu@users.noreply.github.com>
2024-03-20 17:41:52 +0100

    feat: init foldLeft, is missing

--- src/main/scala/task/Task.scala
+++ src/main/scala/task/Task.scala
@@ -13,5 +13,12 @@ object Task:
       case Teacher(n, c) => Cons(c, Nil())
       case _ => Nil())
 
+  def foldLeft[A, B](l: Sequence[A])(i: B)(acc: (B, A) => A): A = ???
+//    l match
+//      case Cons(h, t) => foldLeft(t)(acc(i, h))(acc)
+//      case _ => Nil()
+
+
+
 
     
--- src/test/scala/task/TaskTest.scala
+++ src/test/scala/task/TaskTest.scala
@@ -1,8 +1,8 @@
 package task
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.Test
-import task.Task.courseOf
+import task.Task.*
 import u02.Modules.Person
 import u02.Modules.Person.Teacher
 import u03.Sequences.Sequence
@@ -18,3 +18,8 @@ class TaskTest:
     val l = Cons(p1, Cons(p2, Cons(p3, Nil())))
     assertEquals(Cons("Mat", Cons("Ita", Cons("His", Nil()))), courseOf(l))
 
+  @Test def testFoldLeft(): Unit =
+    val l = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+    val acc: (Int, Int) => Int = _ - _
+    
+    assertEquals(-16, foldLeft(l)(0)(_ - _))
